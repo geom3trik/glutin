657429e5ab88ccdcc7248fafe92f54393057a36c
George Atkinson <geomyles@yahoo.co.uk>
2021-12-27 22:33:55 +0000

    Basic femtovg multiwindow example

--- glutin_examples/examples/multiwindow.rs
+++ glutin_examples/examples/multiwindow.rs
@@ -1,5 +1,7 @@
 mod support;
 
+use femtovg::{Canvas, Color, Path, Paint};
+use femtovg::renderer::OpenGl;
 use glutin::event::{Event, WindowEvent};
 use glutin::event_loop::{ControlFlow, EventLoop};
 use glutin::window::WindowBuilder;
@@ -16,16 +18,24 @@ fn main() {
         let wb = WindowBuilder::new().with_title(title);
         let windowed_context = ContextBuilder::new().build_windowed(wb, &el).unwrap();
         let windowed_context = unsafe { windowed_context.make_current().unwrap() };
-        let gl = support::load(&windowed_context.context());
+
+        let renderer = OpenGl::new(|s| windowed_context.context().get_proc_address(s) as *const _)
+        .expect("Cannot create renderer");
+        let mut canvas = Canvas::new(renderer).expect("Cannot create canvas");
+
+        let size = windowed_context.window().inner_size();
+        canvas.set_size(size.width, size.height, 1.0);
+
+        //let gl = support::load(&windowed_context.context());
         let window_id = windowed_context.window().id();
         let context_id = ct.insert(ContextCurrentWrapper::PossiblyCurrent(
             ContextWrapper::Windowed(windowed_context),
         ));
-        windows.insert(window_id, (context_id, gl, index));
+        windows.insert(window_id, (context_id, canvas, index));
     }
 
     el.run(move |event, _, control_flow| {
-        println!("{:?}", event);
+        //println!("{:?}", event);
         match event {
             Event::LoopDestroyed => return,
             Event::WindowEvent { event, window_id } => match event {
@@ -42,16 +52,34 @@ fn main() {
                 }
                 _ => (),
             },
+
+            Event::MainEventsCleared => {
+                for (_, window) in &windows {
+                    let windowed_context = ct.get_current(window.0).unwrap();
+                    windowed_context.windowed().window().request_redraw();
+                }
+            }
+
             Event::RedrawRequested(window_id) => {
-                let window = &windows[&window_id];
+                if let Some(window) = &mut windows.get_mut(&window_id) {
+    
+                    let windowed_context = ct.get_current(window.0).unwrap();
 
-                let mut color = [1.0, 0.5, 0.7, 1.0];
-                color.swap(0, window.2 % 3);
+                    let size = windowed_context.windowed().window().inner_size();
 
-                let windowed_context = ct.get_current(window.0).unwrap();
+                    //window.1.set_size(size.width, size.height, 1.0);
+                    window.1.clear_rect(0, 0, size.width, size.height, Color::rgb(255, 255, 255));
 
-                window.1.draw_frame(color);
-                windowed_context.windowed().swap_buffers().unwrap();
+
+                    let mut path = Path::new();
+                    path.rect(0.0, 0.0, 50.0, 50.0);
+                    window.1.fill_path(&mut path, Paint::color(Color::rgb(200, 50, 50)));
+    
+                    window.1.flush();
+                    windowed_context.windowed().swap_buffers().unwrap();
+
+                }
+
             }
             _ => (),
         }
